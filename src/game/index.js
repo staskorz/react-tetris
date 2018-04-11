a1ad4a66c87a020ca822f133f6025e740e9c2178
staskorz <staskorz@gmail.com>
2018-04-11 22:59:17 +0300

    Make the board just a part of game state

--- src/game/index.js
+++ src/game/index.js
@@ -1,8 +1,8 @@
 import { BOARD_HEIGHT, BOARD_WIDTH } from "../constants"
 
 // creates matrix (2d array) of BOARD_HEIGHT x BOARD_WIDTH
-const defaultState = Array(BOARD_HEIGHT)
+const emptyBoard = Array(BOARD_HEIGHT)
 	.fill(null)
 	.map(() => Array(BOARD_WIDTH).fill(null))
 
-export default (state = defaultState, action = null) => state
+export default (state = { board: emptyBoard }, action = null) => state
--- src/game/index.test.js
+++ src/game/index.test.js
@@ -2,13 +2,13 @@ import { BOARD_HEIGHT, BOARD_WIDTH } from "../constants"
 import game from "./index"
 
 describe("game", () => {
-	it("creates a game board matrix of BOARD_HEIGHT x BOARD_WIDTH", () => {
-		const matrix = game()
+	it("creates an empty game board matrix of BOARD_HEIGHT x BOARD_WIDTH", () => {
+		const { board } = game()
 
-		expect(matrix[0][0]).toBeNull()
-		expect(matrix[BOARD_HEIGHT - 1][BOARD_WIDTH - 1]).toBeNull()
+		expect(board[0][0]).toBeNull()
+		expect(board[BOARD_HEIGHT - 1][BOARD_WIDTH - 1]).toBeNull()
 
-		expect(matrix[BOARD_HEIGHT]).toBeUndefined()
-		expect(matrix[BOARD_HEIGHT - 1][BOARD_WIDTH]).toBeUndefined()
+		expect(board[BOARD_HEIGHT]).toBeUndefined()
+		expect(board[BOARD_HEIGHT - 1][BOARD_WIDTH]).toBeUndefined()
 	})
 })
